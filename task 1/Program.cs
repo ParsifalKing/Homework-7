195aee43daf974fdf3a69b7936c0f2db2ffbf5b5
Davron Ziyoev <parsifalking0@gmail.com>
2023-12-20 21:48:46 +0500

    Homework 7

---
+++ task 1/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Teacher
+{
+    // fields
+    string fullName;
+    string subject;
+    int experience;
+    // constructors
+    public Teacher()
+    {  }
+    public Teacher(string name, string _subject)
+    { 
+     fullName = name;
+     subject = _subject;
+    }
+    public Teacher(string name, string _subject, int _experience)
+    { 
+     fullName = name;
+     subject = _subject;
+     experience = _experience;
+    }
+    // properties
+    public string FullName { 
+        get { return fullName;}
+        set { fullName = value;} 
+    }
+    public string Subject { 
+        get { return subject;}
+        set { subject = value;} 
+    }
+    public int Experience { 
+        get { return experience;}
+        set { experience = value;} 
+    }
+    // methods
+    string Teach()
+    {
+        return $"{fullName} is teaching {subject} ";
+    }
+    void SetExperience(int years)
+    {
+        experience = years;
+    }
+    int GetExperience()
+    {
+        return experience; 
+    }
+}
---
+++ task 2/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Student
+{
+    // fields
+    string fullName;
+    int age;
+    int grade;
+    // constructors
+    public Student()
+    {  }
+    public Student(string name, int _age)
+    { 
+     fullName = name;
+     age = _age;
+    }
+    public Student(string name, int _age, int _grade)
+    { 
+     fullName = name;
+     age = _age;
+     grade = _grade;
+    }
+    // properties
+    public string FullName { 
+        get { return fullName;}
+        set { fullName = value;} 
+    }
+    public int Age { 
+        get { return age;}
+        set { age = value;} 
+    }
+    public int Grade { 
+        get { return grade;}
+        set { grade = value;} 
+    }
+    // methods
+    void Study()
+    {
+        System.Console.WriteLine( $"{fullName} is studying");
+    }
+    void SetGrade(int newGrade)
+    {
+        grade = newGrade;
+    }
+    int GetGrade()
+    {
+        return grade; 
+    }
+}
